2328884a5761fafae6ed5955f75bac5da7dcc315
abbesdjanati-sudo <abbesdjanati@gmail.com>
2026-09-20 20:40:08 +0100

    Change MainActivity to extend Activity instead of ComponentActivity

--- app/src/main/java/com/goldbot/android/MainActivity.kt
+++ app/src/main/java/com/goldbot/android/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.goldbot.android
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
