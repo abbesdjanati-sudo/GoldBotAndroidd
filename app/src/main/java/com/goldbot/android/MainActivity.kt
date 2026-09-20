ff235fae862849d6ed69375be8d5238b4815615e
abbesdjanati-sudo <abbesdjanati@gmail.com>
2026-09-20 20:37:08 +0100

    Add TextView to display 'Gold Bot' in MainActivity

--- app/src/main/java/com/goldbot/android/MainActivity.kt
+++ app/src/main/java/com/goldbot/android/MainActivity.kt
@@ -2,10 +2,17 @@ package com.goldbot.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Gold Bot"
+        textView.textSize = 28f
+
+        setContentView(textView)
     }
 }
